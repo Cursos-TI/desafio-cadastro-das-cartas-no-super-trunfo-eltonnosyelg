a23313887616fa4f44657f3079387f639783d719
Elton Gleyson <104181058+eltonnosyelg@users.noreply.github.com>
2025-09-01 23:56:13 +0000

    confirmacao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,9 @@
 //Teste larissa
 
 int main() {
-    printf("teste");
+    
+    printf("teste\n");
+ 
     
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
